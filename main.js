525e14a26903272c2e25abd394ed771228a7bc3c
bluesea1234 <difficulttask@outlook.com>
2020-12-23 22:07:17 +0800

    initial axios and browserify

---
+++ main.js
@@ -0,0 +1,12 @@
+const uniq = require('uniq');
+const axios = require('axios');
+
+const data = [1, 2, 3, 4, 4, 3, 5 ];
+console.log(uniq(data));
+
+axios.get('https://jsonplaceholder.typicode.com/posts').then(function(response) {
+  if(response.status === 200) {
+    const postData = response.data;
+    console.log(postData);
+  }
+})
