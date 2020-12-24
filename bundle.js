7dbdde7d2ca7aba2f1357918e7381ec0e610964b
bluesea1234 <difficulttask@outlook.com>
2020-12-24 08:59:07 +0800

    2dn sub branch

--- bundle.js
+++ bundle.js
@@ -195,8 +195,8 @@ axios.get('https://jsonplaceholder.typicode.com/posts').then(function(response) 
   if(response.status === 200) {
     const postData = response.data;
     console.log(postData);
-    
-    localStorage.setItem('postData', postData);
+
+    // localStorage.setItem('postData', postData);
   }
 })
 
